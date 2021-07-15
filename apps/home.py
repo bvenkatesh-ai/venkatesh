28945f878b386eb756e5a60775c9467b291c56fb
bvenkatesh-ai <56408803+bvenkatesh-ai@users.noreply.github.com>
2021-07-15 13:35:17 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -4,8 +4,10 @@ from io import BytesIO
 import os
 def app():
     st.header("Venkatesh Boddu")
-
-    with open("data/cv.pdf","rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf">"""
-        st.markdown(pdf_display, unsafe_allow_html=True)
+    st.write("CV uploaded soon")
+    cv="Not done"
+    if cv=="done":
+        with open("data/cv.pdf","rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+            pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf">"""
+            st.markdown(pdf_display, unsafe_allow_html=True)
