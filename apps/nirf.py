24e4093cac443a7393c876802b7237d817fd47d1
bvenkatesh-ai <56408803+bvenkatesh-ai@users.noreply.github.com>
2021-07-15 21:27:18 +0530

    Update nirf.py

--- apps/nirf.py
+++ apps/nirf.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+sns.set_context("talk")
+sns.set_style("darkgrid")
 @st.cache
 def load_data():
     data = pd.read_csv("data/nirf_2020.csv")
@@ -11,7 +12,7 @@ def load_data():
 
 def app():
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-    sel = st.radio("",("About Project","Explore Project"))
+    sel = st.radio("",("About Project","Statistics","Explore Project"))
     if sel =="About Project":
         about_nirf="""
          The National Institutional Ranking Framework (NIRF) launched
@@ -21,8 +22,55 @@ def app():
          Analysis of of variour factors making the institutions better than others.
          """
         st.markdown(about_nirf,unsafe_allow_html=True)
+
+    #Load data
+    df = load_data()
+    # Melt the dataframe
+    dfm = df.melt(id_vars=['Name', 'City', 'State','Category','Rank'],var_name="Parameter", value_name="Parameter_score")
+    dfm
+
+    #para is sel_state,var is cat
+    def plot_count(para,var,tp):
+        """This function plot institutes count and score in the selected state"""
+        col3,col4 = st.beta_columns(2)
+        para_values = ["All"]+list(df[para].unique())
+        sel_para = col3.selectbox("",para_values)
+        if sel_para =="All":
+            df_filter = df
+        else:
+            df_filter = df[df[para]==sel_para]
+
+
+        fig1, ax1 = plt.subplots(figsize=tp)
+        #if st.button("Show data",key=var):
+        #    st.write(df_filter)
+        ax1 = sns.countplot(y=var,data=df_filter)
+        ax1.set_title("Number of institutes alloted score",fontdict={'fontsize': 18,'color':'red'})
+        #col3.markdown("<br>",unsafe_allow_html=True)
+        col3.pyplot(fig1)
+
+        fig2, ax2 = plt.subplots(figsize=tp)
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:column;}</style>', unsafe_allow_html=True)
+
+        p_type =col4.radio("Graph type",["Bar","Box"],key=var)
+        if p_type=="Box":
+            ax2 = sns.boxplot(x="Score", y=var, data=df_filter)
+            ax2.set_title("Box plot for the institutes",fontdict={'fontsize': 18,'color':'red'})
+        else:
+            ax2 = sns.barplot(x="Score", y=var, data=df_filter)
+            ax2.set_title("Bar plot for the institutes",fontdict={'fontsize': 18,'color':'red'})
+        col4.pyplot(fig2)
+    if sel == "Statistics":
+        st.markdown("<h4>Statistics of the institute scores<h4>",unsafe_allow_html=True)
+        with st.beta_expander("State wise"):
+            plot_count("State","Category",(4,6))
+        with st.beta_expander("Category wise"):
+            plot_count("Category","State",(8,14))
+        st.markdown("<h4>Factors effecting Score<h4>",unsafe_allow_html=True)
+        
+
     if sel == "Explore Project":
-        df = load_data()
+
         sel_cat=st.selectbox('Category',df['Category'].unique())
         df = df.query('Category==@sel_cat')
         df = df.drop(['Category'],axis=1)
