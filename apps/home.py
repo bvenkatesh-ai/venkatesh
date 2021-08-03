34efe0a12c9d7b290962b3079e6526e67bf68063
bvenkatesh-ai <56408803+bvenkatesh-ai@users.noreply.github.com>
2021-08-03 07:35:40 +0530

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -2,12 +2,17 @@ import streamlit as st
 import base64
 from io import BytesIO
 import os
+from streamlit_style.style import embed_html
+import streamlit.components.v1 as components
+
+def cv():
+    with open("data/cv.pdf","rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf">"""
+        st.markdown(pdf_display, unsafe_allow_html=True)
+def local_html(file_name):
+    with open(file_name) as f:
+        #st.markdown(f.read(), unsafe_allow_html=True)
+        components.html(f"{f.read()}",height=800)
 def app():
-    st.header("Venkatesh Boddu")
-    st.write("CV uploaded soon")
-    cv="Not done"
-    if cv=="done":
-        with open("data/cv.pdf","rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}" width="600" height="800" type="application/pdf">"""
-            st.markdown(pdf_display, unsafe_allow_html=True)
+    local_html('skills.html')
