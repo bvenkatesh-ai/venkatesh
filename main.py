73afc7296a2711b46244b2577a86327edb1a2ab4
bvenkatesh-ai <bvenkatesh.ai@gmail.com>
2021-08-05 10:32:37 +0530

    Update main file
    Update the main file

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import streamlit as st
-from apps import model, home, nirf, iit
+from apps import model, home, project, blog
 from PIL import Image
 image = Image.open('data/Boddu_Venkatesh1.jpg')
 
 st.set_page_config(page_title = "Aspiring Data Scientist",page_icon = image,layout="wide")
 model.hide_menu()
-app = model.MultiPage()
+app = model.MultiPage('radio','sidebar')
 app.add_page("Home", home.app)
-app.add_page("NIRF Analytics",nirf.app)
-app.add_page("IIT Seat Prediction",iit.app)
+app.add_page("Projects",project.app)
+app.add_page("Blog", blog.app)
+
+
 app.run()
-style = """
-    <style>
-      # MainMenu {visibility: hidden;}
-      footer {visibility: hidden;}
-     .stApp { bottom: 105px; }
-    </style>
-    """
-st.markdown(style, unsafe_allow_html=True)
-#ft = model.CustomFooter()
-#ft.add_text("Linkedin",h_link="https://www.linkedin.com/in/bvenkatesh-ai/")
-#ft.add_text("Github",h_link="https://github.com/bvenkatesh-ai")
-#ft.generate_footer()
